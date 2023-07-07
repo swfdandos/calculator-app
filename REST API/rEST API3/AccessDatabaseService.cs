34e5d8cb9a657bf51524e0c558e4086895d3af30
swfdandos <burhan.bozdag@ogr.deu.edu.tr>
2023-07-07 21:37:29 +0300

    rest api some changes

---
+++ REST API/rEST API3/AccessDatabaseService.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.OleDb;
+
+public class AccessDatabaseService
+{
+    private readonly string _connectionString;
+
+    public AccessDatabaseService(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public void InsertOperation(string operation)
+    {
+        using (var connection = new OleDbConnection(_connectionString))
+        {
+            connection.Open();
+            using (var command = new OleDbCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = "INSERT INTO table (Operation) VALUES (@Operation)";
+                command.Parameters.AddWithValue("@Operation", operation);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public List<string> GetOperations()
+    {
+        List<string> operations = new List<string>();
+
+        using (var connection = new OleDbConnection(_connectionString))
+        {
+            connection.Open();
+            using (var command = new OleDbCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = "SELECT Operation FROM table";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string operation = reader.GetString(0);
+                        operations.Add(operation);
+                    }
+                }
+            }
+        }
+
+        return operations;
+    }
+}
