4fc1c45bc9e27076e4c67631997c7e565a30bb00
swfdandos <burhan.bozdag@ogr.deu.edu.tr>
2023-07-13 12:55:46 +0300

    message queue
    mq da yazdığım kodlar ve metodlarda bilmedeiğim  hatalar  alıyorum.

--- REST API/lastapi3/lastapi3/Controllers/HomeController.cs
+++ REST API/lastapi3/lastapi3/Controllers/HomeController.cs
@@ -1,16 +1,24 @@
 ﻿using lastapi3.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace lastapi3.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IQueueClient _queueClient;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            string connectionString = "ServiceBusConnectionString";
+            string queueName = "calculatormq";
+            _queueClient = new QueueClient(connectionString, queueName);
         }
 
         public IActionResult Index()
@@ -18,9 +26,58 @@ namespace lastapi3.Controllers
             return View();
         }
 
-        public IActionResult Privacy() 
+        public IActionResult Privacy()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(int num1, int num2, string operation)
+        {
+            decimal result;
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                default:
+                    return BadRequest("Geçersiz işlem");
+            }
+
+            // Hesap sonucunu Service Bus kuyruğuna gönderme
+            string messageToSend = result.ToString();
+            SendMessage(messageToSend);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task SendMessage(string message)
+        {
+            var encodedMessage = new Message(Encoding.UTF8.GetBytes(message));
+            await _queueClient.SendAsync(encodedMessage);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ReceiveMessage()
+        {
+            var receivedMessage = await _queueClient.ReceiveAsync();
+            if (receivedMessage != null)
+            {
+                var messageBody = Encoding.UTF8.GetString(receivedMessage.Body);
+                await _queueClient.CompleteAsync(receivedMessage.SystemProperties.LockToken);
+                return Ok(messageBody);
+            }
+            return NoContent();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ REST API/lastapi3/lastapi3/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lastapi3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // Değerleri döndürme
+            return Ok(new string[] { "Value1", "Value2" });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            // Belirli bir değeri döndürme
+            var value = $"Value {id}";
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            // Yeni bir değer ekleyin
+            // Veritabanına kaydetme vb. işlemleri burada yapabilirsiniz
+            return CreatedAtAction(nameof(Get), new { id = 1 }, value);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+            // Değerin güncellenmesi
+            // Veritabanında güncelleme vb. işlemleri 
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // Değeri silme
+            // Veritabanından silme vb. işlemleri 
+            return NoContent();
+        }
+    }
+}
